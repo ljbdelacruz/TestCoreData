5e965f6add736682ecb1a0e7a241b20aea8643b6
ljbdelacruz <ljbdelacruz123@gmail.com>
2018-10-23 11:39:25 +0800

    updates
    code arrangements by extensions, searchbar UI filter the tasks

--- TestCoreData/ViewController.swift
+++ TestCoreData/ViewController.swift
@@ -11,78 +11,31 @@ import CoreData;
 import SVProgressHUD;
 
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
+    
     
     var tasks:[Task] = [];
     let context=(UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
     
     @IBOutlet weak var UITableview: UITableView!
-    
+    @IBOutlet weak var UISBarTask: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.UITableview.delegate=self;
         self.UITableview.dataSource=self;
+        self.UISBarTask.delegate=self;
+        
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask));
-        self.LoadData();
+        self.LoadAllData();
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //customFunc
-    func SaveContent(taskName:String){
-        let newTask=Task(context: self.context);
-        newTask.set(id: "", name: taskName, isDone:false);
-        self.tasks.append(newTask);
-        self.UpdateContent();
-    }
-    func RemoveComplete(){
-        for index in 0..<self.tasks.count{
-            if self.tasks[index].isDone {
-                context.delete(self.tasks[index]);
-            }
-        }
-        self.UpdateContent();
-        self.LoadData();
-    }
-    func UpdateContent(){
-        do{
-            try context.save();
-        }catch{
-            print("ERROR saving data");
-        }
-        self.UITableview.reloadData();
-    }
-    func LoadData(){
-        let request:NSFetchRequest<Task>=Task.fetchRequest();
-        do{
-            self.tasks=try context.fetch(request);
-            SVProgressHUD.show();
-        }catch{
-            print("has error \(error.localizedDescription)");
-        }
-        SVProgressHUD.dismiss();
-        self.UITableview.reloadData();
-    }
-    //tableView
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tasks.count;
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
-        cell.textLabel?.text = self.tasks[indexPath.row].taskname!;
-        cell.accessoryType = self.tasks[indexPath.row].isDone ? .checkmark : .none;
-        return cell;
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        tableView.deselectRow(at: indexPath, animated: true)
-        self.tasks[indexPath.row].isDone = !self.tasks[indexPath.row].isDone;
-        self.UpdateContent();
-    }
-    
     //BtnEvents
     @IBAction func DeleteCompleteOnClick(_ sender: Any) {
+        self.EndSearchEdit();
         let alert=UIAlertController(title: "Remove Complete Task", message: "Are you sure you want to remove it?", preferredStyle: .alert)
         let yesAction=UIAlertAction(title: "Yes", style: .default, handler: {
             (action) in
@@ -97,6 +50,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     @IBAction func AddOnClick(_ sender: Any){
         var taskNameTF:UITextField?;
+        self.EndSearchEdit()
         let alert=UIAlertController(title: "Add Task", message: "", preferredStyle: .alert)
         alert.addTextField(configurationHandler: {
             (tf) in
@@ -116,10 +70,94 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         present(alert, animated: true, completion: nil)
     }
     
+}
+
+
+//MARK: -UITableView Functionalities
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    //tableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count;
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
+        cell.textLabel?.text = self.tasks[indexPath.row].taskname!;
+        cell.accessoryType = self.tasks[indexPath.row].isDone ? .checkmark : .none;
+        return cell;
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated: true)
+        self.EndSearchEdit();
+        self.tasks[indexPath.row].isDone = !self.tasks[indexPath.row].isDone;
+        self.UpdateContent();
+    }
+}
+
+
+//MARK:- Searchbar methods
+extension ViewController:UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request:NSFetchRequest<Task>=Task.fetchRequest();
+        //cd makes the case insensitive to match wether its lowercase or uppercase
+        if searchBar.text!.count > 0 {
+            let predicate=NSPredicate(format: "taskname CONTAINS[cd] %@", searchBar.text!);
+            request.predicate=predicate;
+            let sorter=NSSortDescriptor(key: "taskname", ascending: true);
+            request.sortDescriptors=[sorter];
+            self.LoadData(request: request);
+        }else{
+            self.LoadAllData();
+        }
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.LoadAllData();
+    }
+    func EndSearchEdit(){
+        self.UISBarTask.endEditing(true);
+    }
+}
+
+
+//MARK: -CoreDataMethods
+extension ViewController{
+    //customFunc
+    func SaveContent(taskName:String){
+        let newTask=Task(context: self.context);
+        newTask.set(id: "", name: taskName, isDone:false);
+        self.tasks.append(newTask);
+        self.UpdateContent();
+    }
+    func RemoveComplete(){
+        for index in 0..<self.tasks.count{
+            if self.tasks[index].isDone {
+                context.delete(self.tasks[index]);
+            }
+        }
+        self.UpdateContent();
+        self.LoadAllData();
+    }
+    func UpdateContent(){
+        do{
+            try context.save();
+        }catch{
+            print("ERROR saving data");
+        }
+        self.UITableview.reloadData();
+    }
+    func LoadAllData(){
+        let request:NSFetchRequest<Task>=Task.fetchRequest();
+        self.LoadData(request: request);
+    }
     
-    
-    
-    
-    
+    func LoadData(request:NSFetchRequest<Task>){
+        SVProgressHUD.show();
+        do{
+            self.tasks=try context.fetch(request);
+        }catch{
+            print("has error \(error.localizedDescription)");
+        }
+        SVProgressHUD.dismiss();
+        self.UITableview.reloadData();
+    }
 }
 
